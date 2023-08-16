24e987b49198115693d56b1fa2fefcb237500556
enocquet <emmanuelnocquet8@gmail.com>
2023-08-16 12:19:12 +0200

    providers typo

--- providers.tf
+++ providers.tf
@@ -13,12 +13,12 @@ terraform {
       version = "2.20.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "terraform-state"
-    storage_account_name = "csmterraformstate"
-    container_name       = "${var.dns_record}-tfstate"
-    key                  = "terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "terraform-state"
+  #   storage_account_name = "csmterraformstate"
+  #   container_name       = "${var.dns_record}-tfstate"
+  #   key                  = "terraform.tfstate"
+  # }
   required_version = ">= 1.3.9"
 }
 
