15c15e12135355c24116c5f39b5c8eda2168be28
enocquet <emmanuelnocquet8@gmail.com>
2023-08-16 12:16:52 +0200

    init

---
+++ main.tf
@@ -0,0 +1,60 @@
+
+
+module "cosmotech-prerequisites" {
+  source           = "Cosmo-Tech/cosmotech-prerequisites/azure"
+  version          = "1.1.35"
+  client_id        = var.client_id
+  client_secret    = var.client_secret
+  tenant_id        = var.tenant_id
+  subscription_id  = var.subscription_id
+  platform_url     = var.platform_url
+  identifier_uri   = var.identifier_uri
+  project_stage    = var.project_stage
+  customer_name    = var.customer_name
+  project_name     = var.project_name
+  owner_list       = var.owner_list
+  audience         = var.audience
+  webapp_url       = var.webapp_url
+  dns_zone_name    = var.dns_zone_name
+  dns_zone_rg      = var.dns_zone_rg
+  dns_record       = var.dns_record
+  vnet_iprange     = var.vnet_iprange
+  api_version_path = var.api_version_path
+  resource_group   = var.resource_group
+  image_path       = var.image_path
+  cluster_name     = var.cluster_name
+  create_cosmosdb  = var.create_cosmosdb
+}
+
+module "cosmotech-platform" {
+  source                    = "Cosmo-Tech/cosmotech-platform/azure"
+  version                   = "1.0.37"
+  subscription_id           = var.subscription_id
+  tenant_id                 = var.tenant_id
+  client_id                 = var.client_id
+  client_secret             = var.client_secret
+  cluster_issuer_email      = var.cluster_issuer_email
+  cluster_issuer_name       = var.cluster_issuer_name
+  tls_secret_name           = var.tls_secret_name
+  namespace                 = var.namespace
+  monitoring_namespace      = var.monitoring_namespace
+  api_dns_name              = module.cosmotech-prerequisites.out_fqdn
+  resource_group            = module.cosmotech-prerequisites.out_ip_resource_group
+  platform_sp_client_id     = module.cosmotech-prerequisites.out_platform_sp_client_id
+  platform_sp_client_secret = module.cosmotech-prerequisites.out_platform_sp_client_secret
+  storage_account_key       = module.cosmotech-prerequisites.out_storage_account_key
+  storage_account_name      = module.cosmotech-prerequisites.out_storage_account_name
+  acr_login_password        = module.cosmotech-prerequisites.out_acr_login_password
+  acr_login_server          = module.cosmotech-prerequisites.out_acr_login_server
+  acr_login_username        = module.cosmotech-prerequisites.out_acr_login_username
+  loadbalancer_ip           = module.cosmotech-prerequisites.out_public_ip
+  adx_ingestion_uri         = module.cosmotech-prerequisites.out_adx_ingestion_uri
+  adx_uri                   = module.cosmotech-prerequisites.out_adx_uri
+  cosmos_uri                = module.cosmotech-prerequisites.out_cosmos_uri
+  cosmos_key                = module.cosmotech-prerequisites.out_cosmos_key
+  eventbus_uri              = module.cosmotech-prerequisites.out_eventbus_uri
+  network_adt_clientid      = module.cosmotech-prerequisites.out_networkadt_clientid
+  network_adt_password      = module.cosmotech-prerequisites.out_network_adt_password
+  managed_disk_id           = module.cosmotech-prerequisites.managed_disk_id
+  kube_config               = module.cosmotech-prerequisites.out_aks_phoenix_config
+}
---
+++ variables.tf
@@ -0,0 +1,314 @@
+variable "resource_group" {
+  type    = string
+  default = ""
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "client_id" {
+  type        = string
+  description = "The client id"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "The client secret"
+}
+
+variable "namespace" {
+  type    = string
+  default = "phoenix"
+}
+
+variable "monitoring_namespace" {
+  type    = string
+  default = "phoenix-monitoring"
+}
+
+variable "loadbalancer_ip" {
+  type    = string
+  default = ""
+}
+
+variable "tls_secret_name" {
+  type    = string
+  default = ""
+}
+
+variable "api_dns_name" {
+  type    = string
+  default = ""
+}
+
+variable "cluster_issuer_email" {
+  type = string
+}
+
+variable "cluster_issuer_name" {
+  type    = string
+  default = ""
+}
+
+variable "ingress_nginx_version" {
+  type    = string
+  default = "4.2.1"
+}
+
+variable "acr_login_password" {
+  type    = string
+  default = ""
+}
+
+variable "acr_login_server" {
+  type    = string
+  default = ""
+}
+
+variable "argo_minio_access_key" {
+  type    = string
+  default = ""
+}
+
+variable "acr_login_username" {
+  type    = string
+  default = ""
+}
+
+variable "cosmos_key" {
+  type    = string
+  default = "default"
+}
+
+variable "chart_package_version" {
+  type = string
+}
+
+variable "cosmos_uri" {
+  type    = string
+  default = "default"
+}
+
+variable "adx_uri" {
+  type    = string
+  default = ""
+}
+
+variable "adx_ingestion_uri" {
+  type    = string
+  default = ""
+}
+
+variable "eventbus_uri" {
+  type    = string
+  default = ""
+}
+
+variable "storage_account_key" {
+  type    = string
+  default = ""
+}
+
+variable "storage_account_name" {
+  type    = string
+  default = ""
+}
+
+variable "disk_size_gb" {
+  type    = string
+  default = "64"
+}
+
+variable "disk_sku" {
+  type    = string
+  default = "Premium_LRS"
+}
+
+variable "disk_tier" {
+  type    = string
+  default = "P6"
+}
+
+variable "kubernetes_version" {
+  type    = string
+  default = "1.25.5"
+}
+
+variable "tenant_id" {
+  description = "The tenant id"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "The subscription id"
+  type        = string
+}
+
+variable "platform_url" {
+  description = "The platform url"
+  type        = string
+  default     = ""
+}
+
+variable "identifier_uri" {
+  description = "The platform identifier uri"
+  type        = string
+  default     = ""
+}
+
+variable "project_stage" {
+  description = "The Project stage"
+  type        = string
+  default     = "Dev"
+  validation {
+    condition = contains([
+      "OnBoarding",
+      "Dev",
+      "QA",
+      "IA",
+      "EA",
+      "Doc",
+      "Support",
+      "Demo",
+      "Prod",
+      "PreProd"
+    ], var.project_stage)
+    error_message = "Stage must be either: OnBoarding, Dev, QA, IA, EA, Demo, Prod, PreProd, Doc, Support."
+  }
+}
+
+variable "customer_name" {
+  description = "The customer name"
+  type        = string
+}
+
+variable "project_name" {
+  description = "The project name"
+  type        = string
+}
+
+variable "owner_list" {
+  description = "List of mail addresses for App Registration owners"
+  type        = list(string)
+}
+
+variable "audience" {
+  description = "The App Registration audience type"
+  type        = string
+  validation {
+    condition = contains([
+      "AzureADMyOrg",
+      "AzureADMultipleOrgs"
+    ], var.audience)
+    error_message = "Only AzureADMyOrg and AzureADMultipleOrgs are supported for audience."
+  }
+  default = "AzureADMultipleOrgs"
+}
+
+variable "location" {
+  description = "The Azure location"
+  default     = "West Europe"
+}
+
+variable "webapp_url" {
+  description = "The Web Application URL"
+  type        = string
+  default     = ""
+}
+
+variable "create_restish" {
+  description = "Create the Azure Active Directory Application for Restish"
+  type        = bool
+  default     = true
+}
+
+variable "create_powerbi" {
+  description = "Create the Azure Active Directory Application for PowerBI"
+  type        = bool
+  default     = true
+}
+
+variable "create_webapp" {
+  description = "Create the Azure Active Directory Application for WebApp"
+  type        = bool
+  default     = true
+}
+
+variable "create_publicip" {
+  description = "Create the public IP for the platform"
+  type        = bool
+  default     = true
+}
+
+variable "create_dnsrecord" {
+  description = "Create the DNS record"
+  type        = bool
+  default     = true
+}
+
+variable "create_vnet" {
+  description = "Create the Virtual Network for AKS"
+  type        = bool
+  default     = true
+}
+
+variable "create_secrets" {
+  description = "Create secret for application registrtations"
+  type        = bool
+  default     = true
+}
+
+variable "dns_zone_name" {
+  description = "The DNS zone name to create platform subdomain. Example: api.cosmotech.com"
+  type        = string
+  default     = ""
+}
+
+variable "dns_zone_rg" {
+  description = "The DNS zone resource group"
+  type        = string
+  default     = "phoenix"
+}
+
+variable "dns_record" {
+  description = "The DNS zone name to create platform subdomain. Example: myplatform"
+  type        = string
+}
+
+variable "vnet_iprange" {
+  description = "The Virtual Network IP range. Minimum /26 NetMaskLength"
+  type        = string
+  default     = ""
+}
+
+variable "api_version_path" {
+  description = "The API version path"
+  type        = string
+  default     = "/"
+}
+
+variable "image_path" {
+  type    = string
+  default = "./cosmotech.png"
+}
+
+variable "managed_disk_name" {
+  type    = string
+  default = ""
+}
+
+variable "network_adt_clientid" {
+  type    = string
+  default = ""
+}
+
+variable "network_adt_password" {
+  type    = string
+  default = ""
+}
+
+variable "create_cosmosdb" {
+  type    = bool
+  default = false
+}
